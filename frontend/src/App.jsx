6bfe88b260249bc4c9ba33b92aeae784099e53d0
Walid26619 <kahumawalid@yahoo.com>
2026-03-21 14:23:23 +0300

    final fix to the code before merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { useState } from "react";
 //before editing the code please tell where u have exactly
-
+//verey important 
 function App() {
   const [hover, setHover] = useState(false);
 
