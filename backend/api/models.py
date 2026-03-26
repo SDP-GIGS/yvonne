8fd9fcc035e9b870a0e9f57f4619d01a99fafee3
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-03-26 20:53:53 +0200

    more of these models

--- backend/api/models.py
+++ backend/api/models.py
@@ -14,4 +14,15 @@ class Students(models.Model):
     password=models.CharField(max_length=14)
     cv=models.FileField(upload_to="cvs")
     date_craeted=models.DateTimeField(auto_now_add=True)
+
+class academic_supervisor(models.Model):
+     name=models.CharField(max_length=100)
+     user_id=models.AutoField(primary_key=True)
+     email=models.EmailField()
+     phone=models.CharField(max_length=13)
+     department=models.CharField(max_length=50)
+     office_location=models.CharField(max_length=100)
+     specialization= models.CharField()
+     student=models.ForeignKey(Students, null=True,on_delete=models.SET_NULL)
+
 # Create your models here.
