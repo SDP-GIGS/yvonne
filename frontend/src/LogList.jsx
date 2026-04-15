61bba07d9437d2ad3d007762c6ca39b7f534bb63
Walid26619 <kahumawalid@yahoo.com>
2026-04-15 11:54:17 +0300

    Modified LogList.jsx

--- frontend/src/LogList.jsx
+++ frontend/src/LogList.jsx
@@ -1,22 +1,21 @@
 function LogList({ logs }) {
   return (
-    <div style={{ marginTop: "30px" }}>
-      <h3>Your Submissions</h3>
+    <div>
+      <h2>Submissions</h2>
 
       {logs.length === 0 ? (
-        <p>No submissions yet</p>
+        <p>No logs yet</p>
       ) : (
         logs.map((log, index) => (
           <div
             key={index}
             style={{
               border: "1px solid gray",
-              padding: "10px",
-              margin: "10px auto",
-              width: "300px"
+              margin: "10px",
+              padding: "10px"
             }}
           >
-            <p>{log.text}</p>
+            {log.text && <p>{log.text}</p>}
 
             {log.file && (
               <p>
