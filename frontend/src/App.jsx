3180b6c4ffbced88bf233b75b25cd161561672f9
Walid26619 <kahumawalid@yahoo.com>
2026-04-15 11:21:36 +0300

    Added a basic Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
-import LogForm from "./LogForm";
 import Footer from "./Footer";
+import Dashboard from "./Dashboard";
 
 function App() {
+  const [showDashboard, setShowDashboard] = useState(false);
+
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Features/>
-    <LogForm/>
-    <Footer/>
+      <Navbar />
+
+      {showDashboard ? (
+        <Dashboard goHome={() => setShowDashboard(false)} />
+      ) : (
+        <>
+          <Hero goToDashboard={() => setShowDashboard(true)} />
+          <Features />
+        </>
+      )}
+
+      <Footer />
     </>
   );
 }
+
 export default App;
