6382504b735151b221a8a6fa3af31d9a26bc6d11
kimera-yvonne2 <yvonnekimera2@gmail.com>
2026-04-17 16:22:50 +0300

    changes

--- frontend/src/pages/login/Admindashboard.jsx
+++ frontend/src/pages/login/Admindashboard.jsx
@@ -14,6 +14,7 @@ const Admindashboard = () => {
         fetchAdminData();
     }, []);
 
+    
     if (!adminData) return <p>Loading Analytics...</p>;
 
     return (
@@ -40,6 +41,7 @@ const Admindashboard = () => {
         </div>
     );
 };
+
 
 export default Admindashboard;
 
