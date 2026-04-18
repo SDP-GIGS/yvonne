cd10e4ca438b63abdef5af2afd965de3253ca636
robinsonmugabi <robinsonmugabi@gmail.com>
2026-04-18 10:41:14 +0300

    serializers

--- backend/evaluations/serializers.py
+++ backend/evaluations/serializers.py
@@ -4,6 +4,7 @@ from .models import AcademicEvaluation
 class EvaluationSerializer(serializers.ModelSerializer):
     # This captures the property we defined in the model
     final_score = serializers.ReadOnlyField(source='final_weighted_score')
+    
 
     class Meta:
         model = AcademicEvaluation
