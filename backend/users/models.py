450bbd4292dfcc7dbbe1b684fbb2fe35fdb50692
Bonitah-nanfuka <bonitahnanfuka@gmail.com>
2026-04-27 04:01:27 -0700

    modified

--- backend/users/models.py
+++ backend/users/models.py
@@ -12,6 +12,7 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='STUDENT')
     phone_number = models.CharField(max_length=15, blank=True)
     
+    
 
     def __str__(self):
         return f"{self.username} ({self.role})" 
