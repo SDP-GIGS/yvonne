175c21a44c2bb0a8a28d9ce828e4698a2f4454d0
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-05-07 00:30:42 +0300

    modified

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import SupervisorReview
 
 
+
 class SupervisorReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = SupervisorReview
