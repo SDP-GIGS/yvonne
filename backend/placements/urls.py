bec056539613359f6a89ecc374b4f04f2fd4235f
alexmutsinzi2021-collab <alexmutsinzi2021@gmail.com>
2026-05-16 20:49:15 +0300

    modified

--- backend/placements/urls.py
+++ backend/placements/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InternshipPlacementViewSet
 
+
 router = DefaultRouter()
 
 # Registering the placement module
