30be477536f18ed9249977aa6a6ddbb1a40c2c14
kimera-yvonne2 <yvonnekimera2@gmail.com>
2026-05-28 10:25:30 +0300

    role changes

--- frontend/src/hooks/use-role.jsx
+++ frontend/src/hooks/use-role.jsx
@@ -3,6 +3,7 @@ import { createContext, useContext, useState } from 'react';
 const RoleContext = createContext({ role: "admin", setRole: () => {} });
 
 export const RoleProvider = ({ children }) => {
+    
     const [role, setRole] = useState("admin");
     return <RoleContext.Provider value={{ role, setRole }}>{children}</RoleContext.Provider>;
 };
